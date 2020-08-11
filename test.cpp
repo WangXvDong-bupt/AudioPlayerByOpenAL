11abc58c4f3571b8c63c6c45f88e6a72a5d22ee1
WangXvDong-bupt <68494698+WangXvDong-bupt@users.noreply.github.com>
2020-08-12 05:37:44 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
 
 
 	cout << "please input url:";
-	//cin >> filepath;
+	cin >> filepath;
 	AudioPlayer audio;
 	audio.openFile(filepath, AVMEDIA_TYPE_AUDIO);
 	audio.audioSetting();
