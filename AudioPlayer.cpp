236a0c4adc518f10b71d5b1fe8a3cb855f18f289
WangXvDong-bupt <68494698+WangXvDong-bupt@users.noreply.github.com>
2020-08-12 09:57:33 +0800

    Update AudioPlayer.cpp

--- AudioPlayer.cpp
+++ AudioPlayer.cpp
@@ -1,6 +1,5 @@
 #include "AudioPlayer.h"
 
-#define RATE 48000
 #define NUMBUFFERS (12)
 #define SERVICE_UPDATE_PERIOD (20)
 using std::cin;
@@ -16,46 +15,46 @@ AudioPlayer::AudioPlayer()
 
 AudioPlayer::~AudioPlayer()
 {
-	//SDL_CloseAudio();//¹Ø±ÕÒôÆµÉè±¸ 
+	//SDL_CloseAudio();//å…³é—­éŸ³é¢‘è®¾å¤‡ 
 	swr_free(&swrCtx);
 }
 
 
 void AudioPlayer::audioSetting()
 {
-	//ÖØ²ÉÑùÉèÖÃÑ¡Ïî-----------------------------------------------------------start
-	//ÊäÈëµÄ²ÉÑù¸ñÊ½
+	//é‡é‡‡æ ·è®¾ç½®é€‰é¡¹-----------------------------------------------------------start
+	//è¾“å…¥çš„é‡‡æ ·æ ¼å¼
 	in_sample_fmt = pCodeCtx->sample_fmt;
-	//Êä³öµÄ²ÉÑù¸ñÊ½ 16bit PCM
+	//è¾“å‡ºçš„é‡‡æ ·æ ¼å¼ 16bit PCM
 	out_sample_fmt = AV_SAMPLE_FMT_S16;
-	//ÊäÈëµÄ²ÉÑùÂÊ
+	//è¾“å…¥çš„é‡‡æ ·çŽ‡
 	in_sample_rate = pCodeCtx->sample_rate;
-	//Êä³öµÄ²ÉÑùÂÊ
-	out_sample_rate = RATE;
-	//ÊäÈëµÄÉùµÀ²¼¾Ö
+	//è¾“å‡ºçš„é‡‡æ ·çŽ‡
+	out_sample_rate = pCodeCtx->sample_rate;
+	//è¾“å…¥çš„å£°é“å¸ƒå±€
 	in_ch_layout = pCodeCtx->channel_layout;
 	if (in_ch_layout <= 0)
 	{
 		in_ch_layout = av_get_default_channel_layout(pCodeCtx->channels);
 	}
-	//Êä³öµÄÉùµÀ²¼¾Ö
+	//è¾“å‡ºçš„å£°é“å¸ƒå±€
 	out_ch_layout = AV_CH_LAYOUT_STEREO;
 
-	//frame->16bit 44100 PCM Í³Ò»ÒôÆµ²ÉÑù¸ñÊ½Óë²ÉÑùÂÊ
+	//frame->16bit 44100 PCM ç»Ÿä¸€éŸ³é¢‘é‡‡æ ·æ ¼å¼ä¸Žé‡‡æ ·çŽ‡
 	swrCtx = swr_alloc();
 	swr_alloc_set_opts(swrCtx, out_ch_layout, out_sample_fmt, out_sample_rate, in_ch_layout, in_sample_fmt,
 		in_sample_rate, 0, NULL);
 	swr_init(swrCtx);
-	//ÖØ²ÉÑùÉèÖÃÑ¡Ïî-----------------------------------------------------------end
+	//é‡é‡‡æ ·è®¾ç½®é€‰é¡¹-----------------------------------------------------------end
 
-	//»ñÈ¡Êä³öµÄÉùµÀ¸öÊý
+	//èŽ·å–è¾“å‡ºçš„å£°é“ä¸ªæ•°
 	out_channel_nb = av_get_channel_layout_nb_channels(out_ch_layout);
 }
 
 
 int AudioPlayer::setAudioSDL()
 {
-	//»ñÈ¡Êä³öµÄÉùµÀ¸öÊý
+	//èŽ·å–è¾“å‡ºçš„å£°é“ä¸ªæ•°
 	out_channel_nb = av_get_channel_layout_nb_channels(out_ch_layout);
 	//SDL_AudioSpec
 	wanted_spec.freq = out_sample_rate;
@@ -63,7 +62,7 @@ int AudioPlayer::setAudioSDL()
 	wanted_spec.channels = out_channel_nb;
 	wanted_spec.silence = 0;
 	wanted_spec.samples = pCodeCtx->frame_size;
-	wanted_spec.callback = fill_audio;//»Øµ÷º¯Êý
+	wanted_spec.callback = fill_audio;//å›žè°ƒå‡½æ•°
 	wanted_spec.userdata = pCodeCtx;
 	if (SDL_OpenAudio(&wanted_spec, NULL) < 0) {
 		printf("can't open audio.\n");
@@ -72,14 +71,14 @@ int AudioPlayer::setAudioSDL()
 }
 
 static Uint8* audio_chunk;
-//ÉèÖÃÒôÆµÊý¾Ý³¤¶È
+//è®¾ç½®éŸ³é¢‘æ•°æ®é•¿åº¦
 static Uint32 audio_len;
 static Uint8* audio_pos;
 
 void  AudioPlayer::fill_audio(void* udata, Uint8* stream, int len) {
 	//SDL 2.0
 	SDL_memset(stream, 0, len);
-	if (audio_len == 0)		//ÓÐÊý¾Ý²Å²¥·Å
+	if (audio_len == 0)		//æœ‰æ•°æ®æ‰æ’­æ”¾
 		return;
 	len = (len > audio_len ? audio_len : len);
 
@@ -103,7 +102,7 @@ void AudioPlayer::feedAudioData(ALuint source, ALuint alBufferId, int out_sample
 
 int AudioPlayer::playByOpenAL()
 {
-	//--------³õÊ¼»¯openAL--------
+	//--------åˆå§‹åŒ–openAL--------
 	ALuint source;
 	ALCdevice* pDevice = alcOpenDevice(NULL);
 	ALCcontext* pContext = alcCreateContext(pDevice, NULL);
@@ -128,31 +127,31 @@ int AudioPlayer::playByOpenAL()
 	alSourcefv(source, AL_VELOCITY, SourceVel);
 	alSourcef(source, AL_REFERENCE_DISTANCE, 50.0f);
 	alSourcei(source, AL_LOOPING, AL_FALSE);
-	//--------³õÊ¼»¯openAL--------
+	//--------åˆå§‹åŒ–openAL--------
 	ALuint alBufferArray[NUMBUFFERS];
 	alGenBuffers(NUMBUFFERS, alBufferArray);
 
 
 
-	//±àÂëÊý¾Ý
+	//ç¼–ç æ•°æ®
 	AVPacket* packet = (AVPacket*)av_malloc(sizeof(AVPacket));
-	//av_init_packet(packet);		//³õÊ¼»¯
-	//½âÑ¹ËõÊý¾Ý
+	//av_init_packet(packet);		//åˆå§‹åŒ–
+	//è§£åŽ‹ç¼©æ•°æ®
 	AVFrame* frame = av_frame_alloc();
-	//´æ´¢pcmÊý¾Ý
-	uint8_t* out_buffer = (uint8_t*)av_malloc(2 * RATE);
+	//å­˜å‚¨pcmæ•°æ®
+	uint8_t* out_buffer = (uint8_t*)av_malloc(2 * out_sample_rate);
 
 	int ret, got_frame, framecount = 0;
 	int out_buffer_size;
 
-	//´ò¿ªÐ´ÈëPCMÎÄ¼þ
+	//æ‰“å¼€å†™å…¥PCMæ–‡ä»¶
 	FILE* fPCM = fopen("output.pcm", "wb");
 
 	while (av_read_frame(pFormatCtx, packet) >= 0)
 	{
 		if (packet->stream_index == index)
 		{
-			//½âÂëAVPacket->AVFrame
+			//è§£ç AVPacket->AVFrame
 			ret = avcodec_decode_audio4(pCodeCtx, frame, &got_frame, packet);
 
 			/*ret = avcodec_send_packet(pCodeCtx, packet);
@@ -161,44 +160,19 @@ int AudioPlayer::playByOpenAL()
 				printf("%s/n", "error");
 				exit(1);
 			}
-			got_frame = avcodec_receive_frame(pCodeCtx, frame);			//output=0¡·success, a frame was returned
+			got_frame = avcodec_receive_frame(pCodeCtx, frame);			//output=0ã€‹success, a frame was returned
 			while ((got_frame = avcodec_receive_frame(pCodeCtx, frame)) == 0) {
-					//¶ÁÈ¡µ½Ò»Ö¡ÒôÆµ»òÕßÊÓÆµ
-					//´¦Àí½âÂëºóÒôÊÓÆµ frame
+					//è¯»å–åˆ°ä¸€å¸§éŸ³é¢‘æˆ–è€…è§†é¢‘
+					//å¤„ç†è§£ç åŽéŸ³è§†é¢‘ frame
 					swr_convert(swrCtx, &out_buffer, 2 * 44100, (const uint8_t**)frame->data, frame->nb_samples);
 			}*/
 			if (got_frame)
 			{
-				swr_convert(swrCtx, &out_buffer, 2 * 44100, (const uint8_t**)frame->data, frame->nb_samples);
+				swr_convert(swrCtx, &out_buffer, 2 * out_sample_rate, (const uint8_t**)frame->data, frame->nb_samples);
 				out_buffer_size = av_samples_get_buffer_size(NULL, out_channel_nb, frame->nb_samples, out_sample_fmt, 1);
 				fwrite(out_buffer, 1, out_buffer_size, fPCM);
 			}
 
-			/*if (ret < 0) {
-				printf("%s", "½âÂëÍê³É");
-			}
-
-			//·Ç0£¬ÕýÔÚ½âÂë
-
-			if (got_frame)
-			{
-				//printf("½âÂë%dÖ¡", framecount++);
-				swr_convert(swrCtx, &out_buffer, 2 * 44100, (const uint8_t**)frame->data, frame->nb_samples);
-				//»ñÈ¡sampleµÄsize
-				out_buffer_size = av_samples_get_buffer_size(NULL, out_channel_nb, frame->nb_samples,
-					out_sample_fmt, 1);
-				//ÉèÖÃÒôÆµÊý¾Ý»º³å,PCMÊý¾Ý
-				audio_chunk = (Uint8*)out_buffer;
-				//ÉèÖÃÒôÆµÊý¾Ý³¤¶È
-				audio_len = out_buffer_size;
-				audio_pos = audio_chunk;
-				//»Ø·ÅÒôÆµÊý¾Ý
-				SDL_PauseAudio(0);
-				while (audio_len > 0)//µÈ´ýÖ±µ½ÒôÆµÊý¾Ý²¥·ÅÍê±Ï!
-					SDL_Delay(10);
-				packet->data += ret;
-				packet->size -= ret;
-			}*/
 		}
 		av_packet_unref(packet);
 	}
@@ -216,7 +190,7 @@ int AudioPlayer::playByOpenAL()
 		feedAudioData(source, alBufferArray[i], out_sample_rate, fpPCM_open);
 	}
 
-	//¿ªÊ¼²¥·Å
+	//å¼€å§‹æ’­æ”¾
 	alSourcePlay(source);
 	ALint iTotalBuffersProcessed = 0;
 	ALint iBuffersProcessed;
@@ -242,7 +216,7 @@ int AudioPlayer::playByOpenAL()
 				alSourcePlay(source);
 			}
 			else {
-				//Í£Ö¹²¥·Å
+				//åœæ­¢æ’­æ”¾
 				break;
 			}
 		}
@@ -268,140 +242,3 @@ int AudioPlayer::playByOpenAL()
 }
 
 
-
-int AudioPlayer::play()
-{
-	//±àÂëÊý¾Ý
-	AVPacket* packet = (AVPacket*)av_malloc(sizeof(AVPacket));
-	//av_init_packet(packet);		//³õÊ¼»¯
-	//½âÑ¹ËõÊý¾Ý
-	AVFrame* frame = av_frame_alloc();
-	//´æ´¢pcmÊý¾Ý
-	uint8_t* out_buffer = (uint8_t*)av_malloc(2 * RATE);
-
-	int ret, got_frame, framecount = 0;
-	//Ò»Ö¡Ò»Ö¡¶ÁÈ¡Ñ¹ËõµÄÒôÆµÊý¾ÝAVPacket
-
-	FILE* fPCM = fopen("output2.pcm", "wb");
-
-	while (av_read_frame(pFormatCtx, packet) >= 0) {
-		if (packet->stream_index == index) {
-				//½âÂëAVPacket->AVFrame
-			ret = avcodec_decode_audio4(pCodeCtx, frame, &got_frame, packet);
-
-			//ret = avcodec_send_packet(pCodeCtx, packet);
-			//if (ret != 0) { printf("%s/n", "error"); }
-			//got_frame = avcodec_receive_frame(pCodeCtx, frame);			//output=0¡·success, a frame was returned
-			//while ((got_frame = avcodec_receive_frame(pCodeCtx, frame)) == 0) {
-			//		//¶ÁÈ¡µ½Ò»Ö¡ÒôÆµ»òÕßÊÓÆµ
-			//		//´¦Àí½âÂëºóÒôÊÓÆµ frame
-			//		swr_convert(swrCtx, &out_buffer, 2 * 44100, (const uint8_t**)frame->data, frame->nb_samples);
-			//}
-
-
-			if (ret < 0) {
-				 printf("%s", "½âÂëÍê³É");
-			}
-
-			//·Ç0£¬ÕýÔÚ½âÂë
-			int out_buffer_size;
-			if (got_frame) {
-				//printf("½âÂë%dÖ¡", framecount++);
-				swr_convert(swrCtx, &out_buffer, 2 * 44100, (const uint8_t**)frame->data, frame->nb_samples);
-				//»ñÈ¡sampleµÄsize
-				out_buffer_size = av_samples_get_buffer_size(NULL, out_channel_nb, frame->nb_samples,
-					out_sample_fmt, 1);
-
-				fwrite(out_buffer, 1, out_buffer_size, fPCM);
-
-				//ÉèÖÃÒôÆµÊý¾Ý»º³å,PCMÊý¾Ý
-				audio_chunk = (Uint8*)out_buffer;
-				//ÉèÖÃÒôÆµÊý¾Ý³¤¶È
-				audio_len = out_buffer_size;
-				audio_pos = audio_chunk;
-				//»Ø·ÅÒôÆµÊý¾Ý
-				SDL_PauseAudio(0);
-				while (audio_len > 0)//µÈ´ýÖ±µ½ÒôÆµÊý¾Ý²¥·ÅÍê±Ï!
-					SDL_Delay(10);
-				packet->data += ret;
-				packet->size -= ret;
-			}
-		}
-		//av_packet_unref(packet);
-	}
-
-	fclose(fPCM);
-
-	av_free(out_buffer);
-	av_frame_free(&frame);
-	//av_free_packet(packet);
-	av_packet_unref(packet);
-	SDL_CloseAudio();//¹Ø±ÕÒôÆµÉè±¸
-	swr_free(&swrCtx);
-	return 0;
-}
-
-int decode(uint8_t* buf, int bufSize, AVPacket* packet, AVCodecContext* codecContext,
-	SwrContext* swr, int dstRate, int dstNbChannels, enum AVSampleFormat* dstSampleFmt) {
-
-
-	unsigned int bufIndex = 0;
-	unsigned int dataSize = 0;
-	int ret = 0;
-	AVPacket* pkt = av_packet_alloc();
-	AVFrame* frame = av_frame_alloc();
-	if (!frame) {
-		cout << "Error allocating the frame" << endl;
-		av_packet_unref(packet);
-		return 0;
-	}
-	while (packet->size > 0) {
-		int gotFrame = 0;
-		int result = avcodec_decode_audio4(codecContext, frame, &gotFrame, packet);
-		if (result >= 0 && gotFrame)
-		{
-			packet->size -= result;
-			packet->data += result;
-			int dstNbSamples = av_rescale_rnd(frame->nb_samples, dstRate, codecContext->sample_rate, AV_ROUND_UP);
-			uint8_t** dstData = NULL;
-			int dstLineSize;
-			if (av_samples_alloc_array_and_samples(&dstData, &dstLineSize, dstNbChannels, dstNbSamples, *dstSampleFmt, 0) < 0) {
-				cerr << "Could not allocate destination samples" << endl;
-				dataSize = 0;
-				break;
-			}
-			int ret = swr_convert(swr, dstData, dstNbSamples, (const uint8_t**)frame->extended_data, frame->nb_samples);
-			if (ret < 0) {
-				cerr << "Error while converting" << endl;
-				dataSize = 0;
-				break;
-			}
-			int dstBufSize = av_samples_get_buffer_size(&dstLineSize, dstNbChannels, ret, *dstSampleFmt, 1);
-			if (dstBufSize < 0) {
-				cerr << "Error av_samples_get_buffer_size()" << endl;
-				dataSize = 0;
-				break;
-			}
-			if (dataSize + dstBufSize > bufSize) {
-				cerr << "dataSize + dstBufSize > bufSize" << endl;
-				dataSize = 0;
-				break;
-			}
-
-			memcpy((uint8_t*)buf + bufIndex, dstData[0], dstBufSize);
-			bufIndex += dstBufSize;
-			dataSize += dstBufSize;
-			if (dstData)
-				av_freep(&dstData[0]);
-			av_freep(&dstData);
-		}
-		else {
-			packet->size = 0;
-			packet->data = NULL;
-		}
-	}
-	av_packet_unref(packet);
-	av_free(frame);
-	return dataSize;
-
-}
